c0fc98c1b9ee22f3975cc21f73fd6e7800b1b329
Roberto Palacios <robpalacios1@hotmail.com>
2023-05-10 11:00:09 -0500

    create file with aritmethic operators

---
+++ 3_tipoOperadores.sh
@@ -0,0 +1,17 @@
+# !/bin/bash
+# Programa para revisar los tipos de operadores
+# Autor: Roberto Palacios
+
+numA=20
+numB=2
+
+echo "Operadores Aritmeticos"
+echo "Numeros A=$numA + B=$numB"
+echo "Sumar A + B =" $((numA + numB))
+echo "Restar A - B=" $((numA - numB))
+echo "Multiplicar A * B=" $((numA * numB))
+echo "Dividir A / B=" $((numA / numB))
+echo "Residuo A % B=" $((numA % numB))
+echo "Entero A // B=" $((numA // numB))
+
+
