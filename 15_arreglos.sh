40b44b24f01a33a12c98b39fe833cdf35d7af08b
Roberto Palacios <robpalacios1@hotmail.com>
2023-05-15 17:34:17 -0500

    create 15_arreglos.sh file

--- 15_arreglos.sh
+++ 15_arreglos.sh
@@ -4,7 +4,7 @@
 
 arrayNumbers=(1, 2, 3, 4, 5, 6)
 arrayChain=(Marco, Antonio, Pedro, Susana)
-arrayRange=({A..Z}{10..20})
+arrayRange=({A..Z} {10..20})
 
 #print all values
 echo "Array Numbers: ${arrayNumbers[*]}"
@@ -21,7 +21,7 @@ echo "Array Numbers: ${arrayNumbers[3]}"
 echo "Array Chain: ${arrayChain[3]}"
 echo "Array Range: ${arrayRange[3]}"
 
-#Add and remove values in a array
+#Add and remove values in array
 arrayNumbers[7]=20
 unset arrayNumber[0]
 echo "Array Numbers: ${arrayNumbers[*]}"
