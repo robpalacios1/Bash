28718e4ef61ba9f8a3e6865d7cade0eafb4e17a8
Roberto Palacios <robpalacios1@hotmail.com>
2023-05-16 11:09:31 -0500

    update code

--- utilityHost.sh
+++ utilityHost.sh
@@ -2,9 +2,17 @@
 # programa para declarar variables, inicializarlas e imprimir valores
 # Author: Roberto Palacios
 
-option=$1
-result=$2
 
-echo "-----\nReto1\n-----"
-echo "la variable option esta inicializada en: $option"
-echo "la variable result esta inicializada en: $result"
+echo -e "\nCreate File..."
+touch log.txt
+
+echo "Registering login..."
+user=$USER
+
+date=$(date +%Y-%m-%d__%H:%M:%S)
+
+echo "$user/$date" >> log.txt
+sleep 2
+
+echo -e "Login registered\n"
+cat log.txt
